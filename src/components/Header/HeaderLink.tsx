4f958fc7390524028b77d60ecad6f5e11e210efc
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-09 13:47:50 +0500

    Rename type to props

--- src/components/Header/HeaderLink.tsx
+++ src/components/Header/HeaderLink.tsx
@@ -1,12 +1,12 @@
 import { useTheme, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
-interface HeaderLinkType {
+interface HeaderLinkProps {
   title: string;
   nav: string;
 }
 
-export default function HeaderLink({ title, nav }: HeaderLinkType) {
+export default function HeaderLink({ title, nav }: HeaderLinkProps) {
   const theme = useTheme();
 
   return (
