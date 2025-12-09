1be68d4969125fb3f7224e8478017821fc539b78
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-09 10:09:50 +0500

    Style App with mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import "./App.css";
+import { Outlet } from "react-router";
+import { Box, Container, useTheme } from "@mui/material";
+import Footer from "./features/Footer/Footer";
 
 function App() {
+  const theme = useTheme();
+
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        background: theme.palette.background.gradient,
+      }}
+    >
+      <Container
+        component="main"
+        sx={{
+          display: "flex",
+          flexGrow: 1,
+        }}
+      >
+        <Outlet />
+      </Container>
+      <Footer />
+    </Box>
   );
 }
 
