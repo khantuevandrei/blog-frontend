bf34856e5c7d947a7793cf7580a1a128a4e2ead4
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-09 14:09:01 +0500

    Update routes with new error element

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,20 @@
 import App from "../App";
 import Error from "../features/Error/Error";
+import Login from "../features/Login/Login";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: <Error />,
     children: [
       {
+        path: "/login",
+        element: <Login />,
+      },
+      {
         path: "*",
-        element: <Error />,
+        element: <Error code={404} message="Page not found" />,
       },
     ],
   },
