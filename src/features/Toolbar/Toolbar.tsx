edfaa95a73976fc68511b1f08c6b6428671e20c6
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-10 16:08:30 +0500

    Add space to toolbar to adjust for fixed position

--- src/features/Toolbar/Toolbar.tsx
+++ src/features/Toolbar/Toolbar.tsx
@@ -1,5 +1,6 @@
 import {
   AppBar,
+  Box,
   Toolbar as MuiToolbar,
   Container,
   alpha,
@@ -13,40 +14,45 @@ export default function Toolbar() {
   const theme = useTheme();
 
   return (
-    <AppBar
-      position="fixed"
-      sx={{
-        boxShadow: 0,
-        bgcolor: "transparent",
-        backgroundImage: "none",
-        mt: { xs: 2, md: 4 },
-      }}
-    >
-      <Container maxWidth="lg">
-        <MuiToolbar
-          variant="dense"
-          disableGutters
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            flexShrink: 0,
-            borderRadius: "16px",
-            backdropFilter: "blur(24px)",
-            border: "1px solid",
-            borderColor: theme.palette.divider,
-            backgroundColor: alpha(theme.palette.background.default, 0.4),
-            boxShadow: 1,
-            p: "8px 12px",
-          }}
-        >
-          <Logo />
-          {/* Large screen sizes */}
-          <NormalToolbar />
-          {/* Small screen sizes */}
-          <SmallToolbar />
-        </MuiToolbar>
-      </Container>
-    </AppBar>
+    <>
+      {/* Spacer to adjust for fixed toolbar */}
+      <Box sx={{ height: { xs: 74, md: 90 } }} />
+      {/* Toolbar */}
+      <AppBar
+        position="fixed"
+        sx={{
+          boxShadow: 0,
+          bgcolor: "transparent",
+          backgroundImage: "none",
+          mt: { xs: 2, md: 4 },
+        }}
+      >
+        <Container maxWidth="lg">
+          <MuiToolbar
+            variant="dense"
+            disableGutters
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              flexShrink: 0,
+              borderRadius: 1,
+              backdropFilter: "blur(24px)",
+              border: "1px solid",
+              borderColor: theme.palette.divider,
+              backgroundColor: alpha(theme.palette.background.default, 0.4),
+              boxShadow: 1,
+              p: "8px 12px",
+            }}
+          >
+            <Logo />
+            {/* Large screen sizes */}
+            <NormalToolbar />
+            {/* Small screen sizes */}
+            <SmallToolbar />
+          </MuiToolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 }
