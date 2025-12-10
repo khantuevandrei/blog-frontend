ec9553cd92b95f412b7e1d94b6b7a7c4f7b27919
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-10 09:17:33 +0500

    Create Post type

---
+++ src/types/Post.ts
@@ -0,0 +1,27 @@
+export interface Author {
+  id: number;
+  username: string;
+}
+
+export interface Comment {
+  id: number;
+  post_id: number;
+  user_id: number;
+  body: string;
+  created_at: string;
+  updated_at: string;
+  author: Author;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  published: boolean;
+  published_at: string | null;
+  created_at: string;
+  updated_at: string;
+  author: Author;
+  comments: Comment[];
+  total_comments: number;
+}
