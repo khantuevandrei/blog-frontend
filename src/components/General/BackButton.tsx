a5e475ab2d99a711bdda9098ddaa5c875e38a33a
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-11 12:42:11 +0500

    Adjust back button size

--- src/components/General/BackButton.tsx
+++ src/components/General/BackButton.tsx
@@ -14,16 +14,16 @@ export default function BackButton({ nav }: { nav: string }) {
         top: -60,
         left: 0,
         [theme.breakpoints.up("sm")]: { top: 0, left: -60 },
-        bgcolor: theme.palette.primary.main,
-        color: theme.palette.primary.contrastText,
-        fontSize: 40,
+        bgcolor: "primary.main",
+        color: "primary.contrastText",
         "&:hover": {
-          bgcolor: theme.palette.primary.light,
+          bgcolor: "primary.dark",
         },
+        fontSize: 40,
         boxShadow: "0 2px 5px rgba(0,0,0,0.3)",
         transition: "all 0.3s ease",
         borderRadius: 100,
-        p: 1.5,
+        p: 1.2,
       }}
     >
       <ArrowBack sx={{ fontSize: 20 }} />
